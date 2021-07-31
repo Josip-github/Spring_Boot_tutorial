f47a1af172e33512ffdabb9bf8b827dd415740e0
Josip-github <josipcota1@gmail.com>
2021-07-31 13:01:18 +0200

    Implementacija klase Student u aplikaciji.

--- src/main/java/com/example/demonelson/DemoNelsonApplication.java
+++ src/main/java/com/example/demonelson/DemoNelsonApplication.java
@@ -1,9 +1,14 @@
 package com.example.demonelson;
 
+import com.example.demonelson.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,13 @@ public class DemoNelsonApplication {
     }
 
     @GetMapping
-    public String hello(){
-        return "Hello world!";
+    public List<Student> hello(){
+        return List.of(new Student(
+                1L,
+                "Mariam",
+                "mariamjamal@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 5),
+                21));
     }
 
 }
