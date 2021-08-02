90e42b7649eaf7806f81b9ded725474449db8ed8
Josip-github <josipcota1@gmail.com>
2021-08-02 09:44:12 +0200

    Delete request

--- src/main/java/com/example/demonelson/student/StudentService.java
+++ src/main/java/com/example/demonelson/student/StudentService.java
@@ -31,4 +31,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("Student with " + studentId + " does not exist.");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
